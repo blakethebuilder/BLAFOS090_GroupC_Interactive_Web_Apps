d058ec3f167631e2c9cede61c620ec3052db39b4
Blake Foster <blake@smartintegrate.co.za>
2023-04-19 23:42:35 +0200

    IWA 4.3 ch1 - enjoying this problem solving, it seems hard at first, but if you break it down into line by line it starts to make sense

---
+++ IWA4/IWA4.3_CH1/scripts.js
@@ -0,0 +1,35 @@
+const date = 2050                                   // as instructed declared this  
+let status = 'parent'                               // declared this as a let 
+let count = 0                                       // declared this as a let 
+
+if (date == 2050) {                                 // corrected the operator to ==
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	let date = 'April'                              // added the let 
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	count = count + 4                               // removed the let 
+
+	if (status == "student") {                      // corrected the operator to ==
+	  console.log('June', 'Youth Day')
+	    count = count + 1                          
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	count = count + 3                                // removed the let to not redefine a global const                         
+
+	if (status == "parent") {                       // corrected the operator to ==
+	  console.log(date, 'Christmas Day')
+	    count = count + 1                            // removed the let to not redefine a global cons
+  }
+
+	console.log(date, 'Day of Goodwill')
+	count = count + 1                                // removed the let to not redefine a global cons
+}
+
+console.log('Your status is:', status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
