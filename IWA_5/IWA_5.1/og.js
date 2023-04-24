37e3e285d8bd6af8ab73f6b1386b12339a752f0b
Blake Foster <blake@smartintegrate.co.za>
2023-04-24 20:56:20 +0200

    revising my if and else statemnets for chalange 5, still having a bug but will get it sorted for the next commit

--- IWA_5/IWA_5.1/og.js
+++ IWA_5/IWA_5.1/og.js
@@ -1,19 +1,19 @@
-//main config//
+//main config
 const FREE_WARNING = "Free shipping only applies to single customer orders";
 const BANNED_WARNING =
   "Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = 0;
 
-//items//
-const shoes = 300 * 2;
+//items
+const shoes = 300 * 1;
 const toys = 100 * 5;
 const shirts = 150 * NONE_SELECTED;
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED;
 
-//changing variables//
-const location = "NAM";
-let currency = "$";
+//changing variables
+const location = "RSA";
+let currency = "null";
 let shipping = null;
 let customers = "1";
 
@@ -26,20 +26,24 @@ if (location === "RSA") {
   currency = "$";
 } else {
   shipping = 800;
+  currency = "$";
 }
 
 //calculate free shipping
-
 if (shoes + toys + batteries + pens + shirts > 1000 && 60) {
   if (location === "NAM" || "RSA") {
     shipping = 0;
+  } else {
+    shipping = 10; // this is not working as intended - if location is NAM or RSA then set shipping to 0 ELSE set shipping to 800 again
   }
 }
 
+//if shipping is 0, check if there is more than customer
 if (shipping === 0 && customers > 1) {
   console.log(FREE_WARNING);
 }
 
+// check if location is NK, and consol log price , Shipping and Total
 location === "NK"
   ? console.log(BANNED_WARNING)
   : console.log(
