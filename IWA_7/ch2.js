fe3c7dc10f3c6280fac2b3399867435694ddf484
Blake Foster <blake@smartintegrate.co.za>
2023-05-04 00:06:40 +0200

    IWA 7 ch1-complete ch2-complete ch3-stillbusy

--- IWA_7/ch2.js
+++ IWA_7/ch2.js
@@ -2,10 +2,10 @@ const nickName = "Timmy";
 const firstName = "Timothy";
 const user = nickName || firstName;
 
-if (nickName == null && firstName == null) {
-  //added in a IF statement to check if the declared const are null,
+if (!nickName && !firstName) {
+  //added in a IF statement to check if the declared ,
   console.log("Good Morning"); //it will only say good morning,
 } else {
   //else, it runs the below console.log
-  console.log(`Good Morning ${user}!`);
+  console.log(`Good Morning ${user}!`); // i had to use backticks `what` - this is not only a new operator
 }
---
+++ IWA_7/ch3.js
@@ -0,0 +1,20 @@
+const leoName = "Leo";
+const leoSurname = "Musvaire";
+const leoBalance = "-9394";
+
+const sarahName = " \nSarah";
+const sarahSurname = "Kleinhans";
+const sarahBalance = "-4582.21000111";
+
+const divider = "\n----------------------------------";
+
+// Only change below this line
+
+const owed =
+  Math.abs(parseInt(leoBalance)) + Math.abs(parseFloat(sarahBalance)); // parsInt to add the variables together
+const leo = `${leoName} ${leoSurname} (Owed R  ${leoBalance})`;
+const sarah = `${sarahName} ${sarahSurname} (Owed R ${sarahBalance})`;
+const total = " \n\tTotal amount owed: R";
+const result = leo + sarah + divider + total + owed.toFixed(2) + divider; // to.fixed to get the decimal and this was
+
+console.log(result);
