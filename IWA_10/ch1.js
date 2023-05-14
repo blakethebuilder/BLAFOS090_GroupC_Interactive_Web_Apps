ac7bf1a08f26dfaba2c649b26cb0b1afb31e4f0e
Blake Foster <blake@smartintegrate.co.za>
2023-05-14 10:10:50 +0200

    fighting this ch 2 but i am defintely getting an good understand ob ibjects, how to store thngs in them and compare them togteher, parse the data between variables ! getting fun !!

--- IWA_10/ch1.js
+++ IWA_10/ch1.js
@@ -55,75 +55,68 @@ const futureId = 9;
 
 console.log(holidays.futureId || `ID ${futureId} not created yet`); // had to parse the future ID with the correct syntax, flippen back ticks ``
 
-copied = {
-  6: {
+let copied = holidays[6];
+
+let correctDate = {
+  tempData: {
     id: 6,
     name: "X-mas",
     date: new Date(`25 December ${currentYear} 00:00`),
-  }, //created a object and reallocated the name x-mas to no.6
+  },
 };
 
-correctDate = copied[6].date;
+const isEarlier = correctDate.tempData.date < holidays[6].date;
 
-isEarlier = correctDate < holidays[6].date;
-
-console.log("New date is earlier:", isEarlier);
+console.log("New date is earlier:", isEarlier, correctDate);
 
 if (isEarlier == true) {
-  copied[6].date = correctDate;
+  copied = correctDate.tempData;
 }
 
-console.log("ID change:", holidays[6].id != copied[6].id);
+console.log("ID change:", holidays[6].id == copied.id);
 
-if (holidays[6].name != copied[6].name) {
-  console.log("Name change", copied[6].name);
+if (holidays[6].name != copied.name) {
+  console.log("Name change", copied.name);
 } else {
-  console.log("Name change:", holidays[6].name == copied[6].name);
+  console.log("Name change:", holidays[6].name == copied.name);
 }
 
-if (holidays[6].date != copied[6].date) {
-  console.log("Date change", copied[6].date);
+if (holidays[6].date != copied.date) {
+  console.log("Date change", copied.date);
 } else {
-  console.log("Date change:", holidays[6].date == copied[6].date);
+  console.log("Date change:", holidays.date == copied.date);
 }
 
 // const firstHolidayTimestamp = Math.min(
-//   holidays[0].date.getTime(),
-//   holidays[1].date.getTime(),
-//   holidays[2].date.getTime(),
-//   holidays[3].date.getTime(),
-//   holidays[4].date.getTime(),
-//   holidays[5].date.getTime(),
-//   holidays[6].date.getTime(),
-//   holidays[7].date.getTime(),
-//   holidays[8].date.getTime()
+//   holidays[0].date.getTime,
+//   holidays[1].date.getTime,
+//   holidays[2].date.getTime,
+//   holidays[3].date.getTime,
+//   holidays[4].date.getTime,
+//   holidays[5].date.getTime,
+//   holidays[6].date.getTime,
+//   holidays[7].date.getTime,
+//   holidays[8].date.getTime
 // );
 
-// const lastHolidayTimestamp = {
-//   Math.max : {
-//     holidays[0].date.getTime(),
-//     holidays[1].date.getTime(),
-//     holidays[2].date.getTime(),
-//     holidays[3].date.getTime(),
-//     holidays[4].date.getTime(),
-//     holidays[5].date.getTime(),
-//     holidays[6].date.getTime(),
-//     holidays[7].date.getTime(),
-//     holidays[8].date.getTime(),
-
-//   }
-
-//   ;
-// }
-// ;
-
-// const firstDay = firstHolidayTimestamp;
-// const firstMonth = firstHolidayTimestamp.getMonth;
-// const lastDay = lastHolidayTimestamp.getDate;
-// const lastMonth = lastHolidayTimestamp.getMonth;
+// const lastHolidayTimestamp = Math.max(
+//   holidays[0].date.getTime,
+//   holidays[1].date.getTime,
+//   holidays[2].date.getTime,
+//   holidays[3].date.getTime,
+//   holidays[4].date.getTime,
+//   holidays[5].date.getTime,
+//   holidays[6].date.getTime,
+//   holidays[7].date.getTime,
+//   holidays[8].date.getTime
+// );
+// const firstDay = new Date(firstHolidayTimestamp).getDate();
+// const firstMonth = new Date(firstHolidayTimestamp).getMonth();
+// const lastDay = new Date(lastHolidayTimestamp).getDate();
+// const lastMonth = new Date(lastHolidayTimestamp).getMonth();
 
 // console.log(`${firstDay}/${firstMonth}/${currentYear}`);
 // console.log(`${lastDay}/${lastMonth}/${currentYear}`);
 
-// // const randomHoliday = holidays[Math.random];
-// // console.log(randomHoliday);
+// const randomHoliday = holidays[Math.random];
+// console.log(randomHoliday);
